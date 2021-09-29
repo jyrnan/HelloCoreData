40eb7d7d083a9c4e2778b6c8b829c1cb4412deb0
jyrnan <yoeking@gmail.com>
2021-09-29 12:43:08 +0800

    Initial Commit

---
+++ HelloCoreData/HelloCoreDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloCoreDataApp.swift
+//  HelloCoreData
+//
+//  Created by jyrnan on 2021/9/29.
+//
+
+import SwiftUI
+
+@main
+struct HelloCoreDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
